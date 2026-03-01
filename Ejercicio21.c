8c410f511c97a65dea1515073fdddf27fc0b086d
Brandon Rivera <119123309+Brariv@users.noreply.github.com>
2026-02-28 21:32:14 -0600

    Improve process handling and execution time measurement
    Refactor process creation and execution timing.

--- Ejercicio21.c
+++ Ejercicio21.c
@@ -15,31 +15,44 @@ int main()
     if(pid1 == 0)
     {
         printf("Soy el proceso hijo 1, mi PID es %d y el PID de mi padre es %d\n", getpid(), getppid());
-    }
-    else
-    {
         pid2 = fork();
         if(pid2 == 0)
         {
             printf("Soy el proceso hijo 2, mi PID es %d y el PID de mi padre es %d\n", getpid(), getppid());
-        }
-        else
-        {
             pid3 = fork();
             if(pid3 == 0)
             {
                 printf("Soy el proceso hijo 3, mi PID es %d y el PID de mi padre es %d\n", getpid(), getppid());
+                for (int i = 0; i < 1000000; i++) {
+                    ;
+                } 
             }
             else
             {
                 printf("Soy el proceso padre, mi PID es %d\n", getpid());
+                for (int i = 0; i < 1000000; i++) {
+                    ;
+                }
+                wait(NULL); 
             }
         }
+        else
+        {
+            printf("Soy el proceso padre, mi PID es %d\n", getpid());
+            for (int i = 0; i < 1000000; i++) {
+                ;
+            }
+            wait(NULL);
+        }
+    }
+    else
+    {
+        wait(NULL);
+        end = clock();
+        cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+        printf("Tiempo de ejecucion: %f segundos\n", cpu_time_used);
     }
     
-    wait(NULL); // Esperar a que los procesos hijos terminen
-    end = clock();
-    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
-    printf("Tiempo de ejecucion: %f segundos\n", cpu_time_used);
+    
 	return 0;
 }
