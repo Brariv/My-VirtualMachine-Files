02d01e6e841eef8fb101514589de4ea45d7fa10a
Brandon Rivera <119123309+Brariv@users.noreply.github.com>
2026-02-28 21:31:54 -0600

    Refactor main function to measure execution time
    Removed process forking and replaced with empty loops for timing.

--- Ejercicio2.c
+++ Ejercicio2.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <time.h>
-#include <unistd.h>
 
 int main()
 {
@@ -9,21 +8,23 @@ int main()
     
     start = clock();
     
-    for(int i=0; i<3; i++)
+    for(int i=0; i<1000000; i++)
     {  
-        if(fork()==0)
-        {
-            printf("Soy el proceso hijo %d, mi PID es %d y el PID de mi padre es %d\n", i+1, getpid(), getppid());
-        }
-        else
-        {
-            printf("Soy el proceso padre, mi PID es %d\n", getpid());
-        }
+        ;
+    }
+
+    for(int i=0; i<1000000; i++)
+    {  
+        ;
+    }
+
+    for(int i=0; i<1000000; i++)
+    {  
+        ;
     }
     
     end = clock();
     cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
     printf("Tiempo de ejecucion: %f segundos\n", cpu_time_used);
 	return 0;
-
 }
